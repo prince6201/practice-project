395eafec4bc9cb5a339420380ddd4db8f9e2928e
prince6201 <princeshingh8434@gmail.com>
2026-08-14 06:04:36 +0000

    new user API endpoint

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +23,41 @@ public class HelloController {
     public String bye() {
         return "Goodbye!";
     }
-}
 
+    @PostMapping("/api/users")
+    public User createUser(@RequestBody User user) {
+        // In a real app you'd save this to a database.
+        // For now, we just return the same user back to confirm it was received.
+        return user;
+    }
+
+    // Simple data model for the request/response body above.
+    public static class User {
+        private String name;
+        private String email;
+
+        public User() {
+        }
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+}
